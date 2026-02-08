9bbd7460f2726fcc8c3122785e95e2dfddeb5e9d
kunwoo park <scv74502@gmail.com>
2026-02-08 15:56:18 +0900

    [init] spring boot 프로젝트 초기화

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "mss_test"
---
+++ src/main/kotlin/org/example/msstest/MssTestApplication.kt
@@ -0,0 +1,11 @@
+package org.example.msstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MssTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<MssTestApplication>(*args)
+}
